017947582d7632df4f93f040637a5c19bd1ac077
Cherika Bodde <st10252644@vcconnect.edu.za>
2025-10-06 20:04:44 +0200

    tests
    tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,5 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
+    testImplementation("junit:junit:4.13.2")
 }
